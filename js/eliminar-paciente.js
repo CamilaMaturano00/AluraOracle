0d9668192aed3013fe49237f18ca4fc39a8393cc
Camila <42713856s@ies9023.net>
2022-01-08 22:17:10 -0300

    Añado funcion de eliminar paciente

---
+++ js/eliminar-paciente.js
@@ -0,0 +1,6 @@
+var pacientes = document.querySelectorAll(".paciente");
+var tabla = document.querySelector("#tabla-pacientes");
+//Con un evento escucho el doble click y con el target.remove procedo a eliminar lo clickeado
+tabla.addEventListener("dblclick", function(event){
+    event.target.parentNode.remove();//Capturo el evento en cada elemento
+});
