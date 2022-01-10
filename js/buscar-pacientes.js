4fb7fc61484a64e9cb7cdc549afb2915973f1192
Camila <42713856s@ies9023.net>
2022-01-09 21:41:25 -0300

    Capturo errores de requerimiento

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -4,13 +4,20 @@ botonBuscar.addEventListener("click",function(){ //Capturo el click
     var xhr= new XMLHttpRequest; //Creo un objeto
     xhr.open("GET","https://alura-es-cursos.github.io/api-pacientes/pacientes.json"); //paso la url de donde quiero extraer
     xhr.addEventListener("load",function(){
-        var respuesta = xhr.responseText;
-
-        var pacientes= JSON.parse(respuesta); //Convierto los string de respuesta en un JSON
-
-        pacientes.forEach(function (paciente){ //Itero entre todos los pacientes 
-            adicionarPaciente(paciente);
-        });
-    })
+        var errorAjax = document.querySelector("#error-ajax");
+        if(xhr.status == 200){
+            errorAjax.classList.add("invisible");
+            var respuesta = xhr.responseText;
+            var pacientes= JSON.parse(respuesta); //Convierto los string de respuesta en un JSON
+            pacientes.forEach(function (paciente){ //Itero entre todos los pacientes 
+              adicionarPaciente(paciente);
+            });
+        }else{
+            errorAjax.classList.remove("invisible");
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
+    });
     xhr.send();
-})
+});
+
